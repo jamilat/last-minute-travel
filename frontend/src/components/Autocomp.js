c6f533cf86492a397a0a993ee1b3b7d43d767627
Pattypan <gulati.parth98@gmail.com>
2022-10-15 16:42:00 -0700

    fixed error:  adding id to autocomplete twice

--- frontend/src/components/Autocomp.js
+++ frontend/src/components/Autocomp.js
@@ -15,7 +15,6 @@ const Autocomp = ({ locat, cities, setLocat, setInputValue, inputValue }) => {
     }}
     InputProps={{ style: { fontSize: 80, height: 90 } }}
     InputLabelProps={{ style: { fontSize: 80 } }}
-    id="controllable-states-demo"
     options={cities}
     sx={{ width: 300, fontSize: 40 }}
     renderInput={(params) => (
